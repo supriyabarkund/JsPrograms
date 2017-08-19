eeb7a05e06c1751261006a3f579f06e9479a1131
supriyabarkund <barkundsupriya28@gmail.com>
2017-08-19 11:13:00 +0530

    Js Programs

---
+++ myapp.js
@@ -0,0 +1,46 @@
+// function addtwono(fno,sno) {
+// 	argument ="Add 2 no ";
+// 	argument += "first add " + fno;
+// 	argument += " then add " + sno ;
+// 	add=fno+sno;
+// 	argument +=" addition of two no is "+ add;
+// 	return argument;
+// }
+// console.log(addtwono(10,20));
+
+//////////////////////////////////////////////////////////////////
+
+// var car = {
+// 	speed :100,
+// 	color : "red",
+// 	drive : function (argument) {
+// 			return drive;
+// 	}
+// }
+// console.log(car);
+
+// var shoppinglist=["supriya","vidya","sonali"];
+
+// console.log(shoppinglist);
+
+///////////////////////////////////////////////////////////////
+
+var car = {
+	maker : "bmw",
+	color : "red",
+	speed : 200,
+	engine : {
+		size : 2.0,
+		color: "yellow",
+		piston : [{maker: "bmw"}, {maker1: "bmw2"}]
+	}
+}
+
+var array =[
+	"string",
+	100,
+	["supriya","sonali"],
+	function(argument) {
+		return	"drive"
+	}
+]
